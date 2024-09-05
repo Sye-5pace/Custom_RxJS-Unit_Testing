4d0a167b5c78e2a62802865b65c3e137bae2e24c
Silas Owu <owusilasyp00@gmail.com>
2024-09-05 07:42:02 -0700

    feat: add comments to task 1

--- src/app/model/custom-operator.service.ts
+++ src/app/model/custom-operator.service.ts
@@ -8,6 +8,14 @@ export class CustomOperatorService {
 
   constructor() { }
 
+  // Task 1: Create a Custom RxJs operator
+  // This method represents a custom RxJs operator named: multiplyBy()
+  // It's uses the pipe operator that allows chaining
+  // of operators with source or inner observables, then uses the map operator
+  // to go through each emitted value from the source observable then transforms
+  //  per the operation within the map operator.In this case,
+  // Multiply each emitted value from the source observable
+  //  by the argument(value) passed as the parameter to custom operator
   multiplyBy(multiplier: number) {
     return (datastream: Observable<number>)=> datastream.pipe(
       map( value => value * multiplier)
