f63440c9c1ca80dd6c674e2f9e4bcd1cfe7119e5
Silas Owu <owusilasyp00@gmail.com>
2024-09-05 13:54:01 -0700

    style: update ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily:{
+      'head': ["Protest Guerrilla", "system-ui"],
+    },
     extend: {},
   },
   plugins: [],
